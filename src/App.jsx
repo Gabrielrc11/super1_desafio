18c0f8acddb5068d5d3bf0e01537a17ed765d6a3
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2025-02-15 15:59:39 -0300

    feat: Adição do carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import './App.css'
 import Header from './components/header/header.jsx'
 import HeaderMenu from './components/headerMenu/headerMenu.jsx'
+import Banner1 from './assets/banner1.svg'
+import Rectangle from './assets/rectangle.svg'
+import { useState, useEffect } from 'react';
+import Banner2 from './assets/banner2.svg'; // Import the second banner
 
-function App() {
-  return (
-    <div>
+  function App() {
+    const [currentBanner, setCurrentBanner] = useState(Banner1);
+    const [showRectangles, setShowRectangles] = useState(false);
+
+    const handleRectangleClick = () => {
+      setCurrentBanner(currentBanner === Banner1 ? Banner2 : Banner1);
+    };
+    useEffect(() => {
+      const interval = setInterval(() => {
+      setCurrentBanner(prevBanner => prevBanner === Banner1 ? Banner2 : Banner1);
+      }, 5000);
+
+      return () => clearInterval(interval);
+    }, []);
+    return (
+      <div>
       <Header className="header"/>
       <HeaderMenu className="header-menu"/>
-    </div>
-  )
-}
+      <div className="banner" onMouseEnter={() => setShowRectangles(true)} onMouseLeave={() => setShowRectangles(false)}>
+        {showRectangles && (
+        <>
+          <img src={Rectangle} alt="Rectangle" className="rectangle1" onClick={handleRectangleClick} />
+          <img src={Rectangle} alt="Rectangle" className="rectangle2" onClick={handleRectangleClick} />
+        </>
+        )}
+        <img src={currentBanner} alt="Banner" className="banner1" />
+      </div>
+      </div>
+    );
+  }
 
 export default App
